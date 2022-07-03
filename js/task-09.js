5e08502ea5f1240876159bc3762e62199ebacdf1
Olena-Zhyzhko <asil198609@gmail.com>
2022-07-03 23:32:44 +0300

    Task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColorEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+
+btnChangeColorEl.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) {
+  const bgColorEl = document.querySelector('.color');
+
+  const colorNow = getRandomHexColor();
+  console.log(colorNow);
+
+  bodyEl.style.backgroundColor = `${colorNow}`;
+  bgColorEl.textContent = `${colorNow}`;
+
+}
