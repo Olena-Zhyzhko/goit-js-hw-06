762a5fa5f86d86ccf465699d3680fcf1baebf6b7
Olena-Zhyzhko <asil198609@gmail.com>
2022-07-03 23:14:54 +0300

    Task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (email === "" || password === "") {
+        window.alert(`Всі поля форми мають бути заповнені`);
+    } else {
+        // Розмістила в цій частині, для того, щоб не скидались вже заповненні поля.
+        const formData = { email, password };
+        console.log(formData);
+
+        event.currentTarget.reset();
+    }
+
+    
+}
