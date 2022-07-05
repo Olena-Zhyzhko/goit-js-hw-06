ffc823fe37a083514891a7824b412689e62629f1
Olena-Zhyzhko <asil198609@gmail.com>
2022-07-05 14:56:46 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,11 +3,14 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', (event) => {
     const simbols = inputEl.value.split("").length;
     console.log(simbols === inputEl.dataset.length);
-
+    
     if (simbols === Number(inputEl.dataset.length)) {
-        inputEl.classList.toggle("valid");
-        inputEl.classList.remove("invalid")
+        inputEl.classList.toggle("valid"); 
+        inputEl.classList.remove("invalid");
     } else {
+        if (inputEl.classList.contains("invalid")) {
+            inputEl.classList.remove("invalid");
+        } 
         inputEl.classList.toggle("invalid");
         inputEl.classList.remove("valid");}
 })
